69a04c9fbb0a38e732f38abafec49445f717e2de
Damir Ivanidze <damir.ivanidze@gmail.com>
2020-07-16 17:08:38 +0300

    change class-wp-bootstrap-navwalker

--- deeplevel/inc/class-wp-bootstrap-navwalker.php
+++ deeplevel/inc/class-wp-bootstrap-navwalker.php
@@ -184,9 +184,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			$atts['target'] = ! empty( $item->target ) ? $item->target : '';
 			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 			// If the item has children, add atts to the <a>.
-			// if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
-			if ( isset( $args->has_children ) && $args->has_children && $args->depth > 1 ) {
-			// if ( isset( $args->has_children ) && $args->has_children && $args->depth > 1 ) {
+			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
 				$atts['href']          = '#';
 				$atts['data-toggle']   = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
