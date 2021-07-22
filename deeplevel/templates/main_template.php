c681b217a0da638ebd44255994c85bdc232d2339
Дамир Иванидзе <damirivanidze@MacBook-Air-Damir.local>
2021-07-22 22:23:28 +0300

    added folder with template and removed front-page

---
+++ deeplevel/templates/main_template.php
@@ -0,0 +1,19 @@
+<?
+/*
+Template Name: Главная
+*/
+
+get_header();
+?>
+
+<section class="hero lozad" data-background-image="<?= get_the_post_thumbnail_url(); ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<? get_footer();?>
